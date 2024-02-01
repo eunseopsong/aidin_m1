5114017aacc5229b1923d3432c71ae25a5e258fc
eunseopsong <lexondms1@g.skku.edu>
2024-02-01 18:08:21 +0900

    edit control_node (3)

--- src/aidin_m1_control_node.cpp
+++ src/aidin_m1_control_node.cpp
@@ -9,6 +9,7 @@
 #include "rosgraph_msgs/msg/clock.hpp"
 
 using namespace std;
+using namespace Eigen;
 
 class JointControl: public rclcpp::Node
 {
@@ -109,27 +110,25 @@ private:
 
     //////////// Method of Undetermined Coefficients using Eigen ////////////
 
-    // void solve(double d, double e, double f, double T, double singularity, double B_val[], double arr[6])
-    // {
-    //     using namespace std;
+    void solve(double d, double e, double f, double T, double singularity, double B_val[], double arr[6])
+    {
+        Eigen::Matrix3d A;  // 3x3 행렬
+        Eigen::Vector3d B;  // 크기 3의 벡터
 
-    //     Matrix3d A;  // 3x3 행렬
-    //     Vector3d B;  // 크기 3의 벡터
+        // 행렬과 벡터 값 설정 (A는 주어진 행렬, B는 상수 벡터)
+        A << (5*pow(singularity, 4)), (4*pow(singularity, 3)), (3*pow(singularity, 2)), pow((T/4), 5), pow((T/4), 4), pow((T/4), 3), 20*pow((T/4), 3), 12*pow((T/4), 2), 6*pow((T/4), 1);
+        B << B_val[0], B_val[1], B_val[2];
 
-    //     // 행렬과 벡터 값 설정 (A는 주어진 행렬, B는 상수 벡터)
-    //     A << (5*pow(singularity, 4)), (4*pow(singularity, 3)), (3*pow(singularity, 2)), pow((T/4), 5), pow((T/4), 4), pow((T/4), 3), 20*pow((T/4), 3), 12*pow((T/4), 2), 6*pow((T/4), 1);
-    //     B << B_val[0], B_val[1], B_val[2];
+        // 선형 시스템 풀기
+        Eigen::Vector3d solution = A.colPivHouseholderQr().solve(B);
 
-    //     // 선형 시스템 풀기
-    //     Vector3d solution = A.colPivHouseholderQr().solve(B);
+        // 결과값 저장
+        double S[6] = {solution[0], solution[1], solution[2], d, e, f};
 
-    //     // 결과값 저장
-    //     double S[6] = {solution[0], solution[1], solution[2], d, e, f};
-
-    //     // 결과값 반환
-    //     for (int i=0; i < 6; i++)
-    //         arr[i] = S[i];
-    // };
+        // 결과값 반환
+        for (int i=0; i < 6; i++)
+            arr[i] = S[i];
+    }
 
     ////////////////// for STandingPhase //////////////////
 
@@ -172,120 +171,144 @@ private:
         return SWValues;
     }
 
+    void SplineTrajectory(double sim_time, double &xVal, double &zVal)
+    {
+        /////////////////////// Initializing ////////////////////////
 
+        double vel_of_body = 1600;
+        double T = 0.5;
+        double length_of_STanding_phase = vel_of_body * T /2;
 
+        double dt = 0.001;
+        double tStart = 0.0;
+        double tEnd = T/2;
 
+        double scap_degree = 0, hip_degree = (40 * M_PI / 180 - M_PI), knee_degree = 90 * M_PI / 180;
+        double height = 1656/5;
+        double scap_length = 80, hip_length = 250, knee_length = 250;
+        double InitailxValues = -61.1902;
 
+        int ST_x_case = 1, SW_x_case = 2, Reverse_x_case = 3;
+        int ST_z_case = 4, SW_z_case = 5, Reverse_z_case = 6;
 
+        // undetermined coefficients of SWxValues (a1*t^5 + b1*t^4 + c1*t^3 + d1*t^2 + e1*t + f1)
+        double d1 = 0, e1 = -(vel_of_body), f1 = -length_of_STanding_phase/2;
+        double singular1 = T/16;
+        double B_val1[3] = {-e1, -(f1 +e1*T/4), 0};
+        double S1[6];
 
+        // undetermined coefficients of SWzValues (a2*t^5 + b2*t^4 + c2*t^3 + d2*t^2 + e2*t + f2)
+        double d2 = 0, e2 = 0, f2 = -height;
+        double singular2 = T/4;
+        double B_val2[3] = {0, -(5*height/6 +f2), 0};
+        double S2[6];
 
+        // Initialize return value
+        double returnXValue = 0, returnZValue = 0;
+        double t = fmod(sim_time, T);
 
+        //////////////////// Divide the Phase /////////////////////
 
-    // Calculation of Bezier Curve
-    double factorial(int n) {
-        if (n <= 1) {
-            return 1;
+        if (t <= T/2) {
+        ////////////////// STandingPhase //////////////////
+        //// Solve x ////
+        // 시간에 따른 x 좌표의 변화 계산
+        std::vector<double> STxValues = CalculateXValues(vel_of_body, tStart, T/2, dt, length_of_STanding_phase);
+
+        //// Solve z ////
+        // 시간에 따른 z 좌표의 변화 계산 // zValue is constant in STanding Phase.
+        std::vector<double> STzValues(T/2/dt, -height);
+
+        } else if (t <= T*(3/4)) {
+        ////////////////// SWingPhase //////////////////
+        //// Solve x ////
+        // solve undetermined coefficients (double S1[6] = {a1, b1, c1, d1, e1, f1};)
+        solve(d1, e1, f1, T, singular1, B_val1, S1);
+
+        std::vector<double> SWxValues = CalculateValues(S1, tStart, T/2, dt, SW_x_case);
+
+        //// Solve z ////
+        // solve undetermined coefficients (double S2[6] = {a2, b2, c2, d2, e2, f2};)
+        solve(d2, e2, f2, T, singular2, B_val2, S2);
+
+        std::vector<double> SWzValues = CalculateValues(S2, tStart, T/2, dt, SW_z_case);
+
         } else {
-            return n * factorial(n-1);
+        ////////////////// ReversePhase //////////////////
+        //// Solve x ////
+        // Reverse of SWingPhase
+        std::vector<double> REVERSExValues = CalculateValues(S1, tStart, T/2, dt, Reverse_x_case);
+
+        //// Solve z ////
+
+        std::vector<double> REVERSEzValues = CalculateValues(S2, tStart, T/2, dt, Reverse_z_case);
         }
+        xVal = returnXValue;
+        zVal = returnZValue;
     }
-
-    double nchoosek(int n, int k) {
-        return factorial(n) / (factorial(k) * factorial(n-k));
-    }
-
-    void bezierEndpoint(double t, double &y, double &z) {
-        q1 = 0, q2 = 40 * M_PI / 180 - M_PI, q3 = 90 * M_PI / 180;          // initial leg angle
-        l1 = 80, l2 = 230, l3 = 230;        // leg length
-        double S = 0.2, V = 1.8*1000;       // S: period of gate cycle, V: gate velocity
-        double D = S*V/2;
-        double pe[2];
-
-        // Points for Making Bezier Curve
-        // pe[0] = l3*(cos(q2)*sin(q1)*sin(q3) + cos(q3)*sin(q1)*sin(q2)) + l1*cos(q1) + l2*sin(q1)*sin(q2);    // x forward kinematics
-        pe[0] = l3*(cos(q2)*cos(q3) - sin(q2)*sin(q3)) + l2*cos(q2);                                            // y forward kinematics
-        pe[1] = l3*(cos(q1)*cos(q2)*sin(q3) + cos(q1)*cos(q3)*sin(q2)) - l1*sin(q1) + l2*cos(q1)*sin(q2);       // z forward kinematics
-
-        double P0[2], P1[2], P2[2], P3[2], P4[2], P5[2], P6[2], P7[2], P8[2];
-        P0[0] = pe[0];              P0[1] = pe[1];
-        P1[0] = pe[0]-D;            P1[1] = pe[1];
-        P2[0] = pe[0]-D-V/10*S;     P2[1] = pe[1];
-        P3[0] = pe[0]-D-V/10*S-10;  P3[1] = 7*pe[1]/8;
-        P4[0] = pe[0];              P4[1] = 7*pe[1]/8;
-        P5[0] = pe[0];              P5[1] = 19*pe[1]/24;
-        P6[0] = pe[0]+D+V/10*S+10;  P6[1] = 5*pe[1]/6;
-        P7[0] = pe[0]+D+V/10*S;     P7[1] = pe[1];
-        P8[0] = pe[0]+D;            P8[1] = pe[1];
-
-
-        double By = 0, Bz = 0;
-        t = fmod(t, 2*S);
-
-        if (t <= S/2) {
-            double p[2][2] = {{P0[0], P0[1]}, {P1[0], P1[1]}};
-            int n = 2, n1 = n - 1;
-
-            for (int i=0; i<=n1; i++) {
-                By += nchoosek(n1, i) * p[i][0] * pow(1 - (t / (S / 2)), n1 - i) * pow(t / (S / 2), i);
-                Bz += nchoosek(n1, i) * p[i][1] * pow(1 - (t / (S / 2)), n1 - i) * pow(t / (S / 2), i);
-            }
-        }
-        else if (t <= 3*S/2) {
-            double p[11][2] = {{P1[0], P1[1]}, {P2[0], P2[1]}, {P3[0], P3[1]}, {P4[0], P4[1]},
-                                {P5[0], P5[1]}, {P6[0], P6[1]}, {P7[0], P7[1]}, {P8[0], P8[1]}};
-            int n = 11, n1 = n - 1;
-
-            for (int i = 0; i <= n1; i++) {
-                By += nchoosek(n1, i) * p[i][0] * pow(1 - ((t - (S / 2)) / S), n1 - i) * pow((t - (S / 2)) / S, i);
-                Bz += nchoosek(n1, i) * p[i][1] * pow(1 - ((t - (S / 2)) / S), n1 - i) * pow((t - (S / 2)) / S, i);
-            }
-        }
-        else {
-            double p[2][2] = {{P8[0], P8[1]}, {P0[0], P0[1]}};
-            int n = 2, n1 = n - 1;
-
-            for (int i = 0; i <= n1; i++) {
-                By += nchoosek(n1, i) * p[i][0] * pow(1 - ((t - (3 * S / 2)) / (S / 2)), n1 - i) * pow((t - (3 * S / 2)) / (S / 2), i);
-                Bz += nchoosek(n1, i) * p[i][1] * pow(1 - ((t - (3 * S / 2)) / (S / 2)), n1 - i) * pow((t - (3 * S / 2)) / (S / 2), i);
-            }
-        }
-
-        y = By;
-        z = Bz;
-    }
-
-
-
-
-
-
-
-
 
 
     void CalculateAndPublishTorque()
     {
         // 발 끝 좌표
         double y, z;
-        bezierEndpoint(sim_time, y, z);
+        SplineTrajectory(sim_time, y, z);
         double x = 79;
 
-        // 조인트 각도 계산
-        double costh1 = (x*l1 + sqrt(x*x*l1*l1 - (x*x + z*z)*(l1*l1 - z*z))) / (x*x + z*z);
+        // // 조인트 각도 계산
+        // double costh1 = (x*l1 + sqrt(x*x*l1*l1 - (x*x + z*z)*(l1*l1 - z*z))) / (x*x + z*z);
 
-        if (costh1 >= -1 && costh1 <= 1)
-        {
-            double th1 = atan2(sqrt(1 - costh1*costh1), costh1);
+        // if (costh1 >= -1 && costh1 <= 1)
+        // {
+        //     double th1 = atan2(sqrt(1 - costh1*costh1), costh1);
 
-            double p_rot_y = y*cos(th1) - z*sin(th1);
-            double p_rot_z = y*sin(th1) + z*cos(th1);
+        //     double p_rot_y = y*cos(th1) - z*sin(th1);
+        //     double p_rot_z = y*sin(th1) + z*cos(th1);
 
-            double th = atan2(p_rot_z, p_rot_y);
-            double l = sqrt(p_rot_y*p_rot_y + p_rot_z*p_rot_z);
+        //     double th = atan2(p_rot_z, p_rot_y);
+        //     double l = sqrt(p_rot_y*p_rot_y + p_rot_z*p_rot_z);
 
-            double th2 = M_PI + th - acos((l2*l2 + l*l - l3*l3) / (2*l2*l));
-            double th3 = M_PI - acos((l2*l2 + l3*l3 - l*l) / (2*l2*l3));
+        //     double th2 = M_PI + th - acos((l2*l2 + l*l - l3*l3) / (2*l2*l));
+        //     double th3 = M_PI - acos((l2*l2 + l3*l3 - l*l) / (2*l2*l3));
 
+
+        //     std_msgs::msg::Float32MultiArray torque_msg;
+        //     torque_msg.data.clear();
+
+        //     torque_msg.data.push_back(ag[0]);
+        //     torque_msg.data.push_back(0);
+        //     torque_msg.data.push_back(0);
+        //     torque_msg.data.push_back(-ag[0]);
+        //     torque_msg.data.push_back(kp[1]*(th2-M_PI/2 - joint2_pos) + kd[1]*(0-joint2_vel));
+        //     torque_msg.data.push_back(kp[2]*(th3 - joint3_pos) + kd[2]*(0-joint3_vel));
+        //     torque_msg.data.push_back(ag[0]);
+        //     torque_msg.data.push_back(0);
+        //     torque_msg.data.push_back(0);
+        //     torque_msg.data.push_back(-ag[0]);
+        //     torque_msg.data.push_back(0);
+        //     torque_msg.data.push_back(0);
+
+        //     pub_torque->publish(torque_msg);
+
+
+        //     std_msgs::msg::Float32MultiArray desiredpos_msg;
+        //     desiredpos_msg.data.clear();
+
+        //     desiredpos_msg.data.push_back(ag[0]);
+        //     desiredpos_msg.data.push_back(0);
+        //     desiredpos_msg.data.push_back(0);
+        //     desiredpos_msg.data.push_back(ag[0]);
+        //     desiredpos_msg.data.push_back(th2-M_PI/2);
+        //     desiredpos_msg.data.push_back(th3);
+        //     desiredpos_msg.data.push_back(ag[0]);
+        //     desiredpos_msg.data.push_back(0);
+        //     desiredpos_msg.data.push_back(0);
+        //     desiredpos_msg.data.push_back(ag[0]);
+        //     desiredpos_msg.data.push_back(0);
+        //     desiredpos_msg.data.push_back(0);
+
+        //     pub_desiredpos->publish(desiredpos_msg);
+        // }
 
             std_msgs::msg::Float32MultiArray torque_msg;
             torque_msg.data.clear();
@@ -294,8 +317,8 @@ private:
             torque_msg.data.push_back(0);
             torque_msg.data.push_back(0);
             torque_msg.data.push_back(-ag[0]);
-            torque_msg.data.push_back(kp[1]*(th2-M_PI/2 - joint2_pos) + kd[1]*(0-joint2_vel));
-            torque_msg.data.push_back(kp[2]*(th3 - joint3_pos) + kd[2]*(0-joint3_vel));
+            torque_msg.data.push_back(0);
+            torque_msg.data.push_back(0);
             torque_msg.data.push_back(ag[0]);
             torque_msg.data.push_back(0);
             torque_msg.data.push_back(0);
@@ -313,8 +336,8 @@ private:
             desiredpos_msg.data.push_back(0);
             desiredpos_msg.data.push_back(0);
             desiredpos_msg.data.push_back(ag[0]);
-            desiredpos_msg.data.push_back(th2-M_PI/2);
-            desiredpos_msg.data.push_back(th3);
+            desiredpos_msg.data.push_back(0);
+            desiredpos_msg.data.push_back(0);
             desiredpos_msg.data.push_back(ag[0]);
             desiredpos_msg.data.push_back(0);
             desiredpos_msg.data.push_back(0);
@@ -323,10 +346,6 @@ private:
             desiredpos_msg.data.push_back(0);
 
             pub_desiredpos->publish(desiredpos_msg);
-        }
-
-
-
     }
 };
 
