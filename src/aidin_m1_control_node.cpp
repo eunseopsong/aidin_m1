b8acdaeb879ae99264ec8d32d50064bcedeedf6f
eunseopsong <lexondms1@g.skku.edu>
2024-02-05 15:10:48 +0900

    경로 생성 문제점 발견 -> 수정 시도 {1}

--- src/aidin_m1_control_node.cpp
+++ src/aidin_m1_control_node.cpp
@@ -89,6 +89,7 @@ public:
     }
 
 private:
+    /////////////////// Initializing ////////////////////
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_jointpos;
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_jointvel;
     rclcpp::Subscription<rosgraph_msgs::msg::Clock>::SharedPtr sub_simtime;
@@ -111,15 +112,15 @@ private:
 
     void solve(double d, double e, double f, double T, double singularity, double B_val[], double arr[6])
     {
-        Eigen::Matrix3d A;  // 3x3 행렬
-        Eigen::Vector3d B;  // 크기 3의 벡터
+        Matrix3d A;  // 3x3 행렬
+        Vector3d B;  // 크기 3의 벡터
 
         // 행렬과 벡터 값 설정 (A는 주어진 행렬, B는 상수 벡터)
         A << (5*pow(singularity, 4)), (4*pow(singularity, 3)), (3*pow(singularity, 2)), pow((T/4), 5), pow((T/4), 4), pow((T/4), 3), 20*pow((T/4), 3), 12*pow((T/4), 2), 6*pow((T/4), 1);
         B << B_val[0], B_val[1], B_val[2];
 
         // 선형 시스템 풀기
-        Eigen::Vector3d solution = A.colPivHouseholderQr().solve(B);
+        Vector3d solution = A.colPivHouseholderQr().solve(B);
 
         // 결과값 저장
         double S[6] = {solution[0], solution[1], solution[2], d, e, f};
@@ -130,10 +131,11 @@ private:
     }
 
     ////////////////// for STandingPhase //////////////////
-    // 시간에 따른 STandingPhase x 좌표의 변화를 반환하는 함수
+    // 시간에 따른 STandingPhase x 좌표의 변화를 저장하는 함수
     double CalculateXValues(double l, double v, double t)
     {
         double returnXValue = (l/2) - v*t;
+
         return returnXValue;
     }
 
@@ -149,66 +151,10 @@ private:
         } else {
             // ReversePhase (x & z)
             returnValue = S[0]*pow(T-t, 5) + S[1]*pow(T-t, 4) + S[2]*pow(T-t, 3) + S[3]*pow(T-t, 2) + S[4]*pow(T-t, 1) + S[5];
+
         }
 
         return returnValue;
-    }
-
-    void SplineTrajectory(double sim_time, double &xVal, double &zVal)
-    {
-        /////////////////////// Initializing ////////////////////////
-
-        double vel_of_body = 1600;
-        double T = 0.5;
-        double length_of_STanding_phase = vel_of_body * T /2;
-        double height = 1656/5;
-
-        // int ST_x_case = 1;
-        int SW_x_case = 2, Reverse_x_case = 3;
-        // int ST_z_case = 4;
-        int SW_z_case = 5, Reverse_z_case = 6;
-
-        // undetermined coefficients of SWxValues (a1*t^5 + b1*t^4 + c1*t^3 + d1*t^2 + e1*t + f1)
-        double d1 = 0, e1 = -(vel_of_body), f1 = -length_of_STanding_phase/2;
-        double singular1 = T/16;
-        double B_val1[3] = {-e1, -(f1 +e1*T/4), 0};
-        double S1[6];
-
-        // undetermined coefficients of SWzValues (a2*t^5 + b2*t^4 + c2*t^3 + d2*t^2 + e2*t + f2)
-        double d2 = 0, e2 = 0, f2 = -height;
-        double singular2 = T/4;
-        double B_val2[3] = {0, -(5*height/6 +f2), 0};
-        double S2[6];
-
-        /////////////////////// Calculate the return Value ////////////////////////
-        // Initialize the return value
-        double returnXValue, returnZValue;
-        double t = fmod(sim_time, T);
-
-        if (t <= T/2) {
-        ////////////////// STandingPhase //////////////////
-        returnXValue = CalculateXValues(length_of_STanding_phase, vel_of_body, t);
-        returnZValue = -height;
-
-        } else if (t <= T/4*3) {
-        ////////////////// SWingPhase //////////////////
-        //// Solve x ////
-        // solve undetermined coefficients (double S1[6] = {a1, b1, c1, d1, e1, f1};)
-        solve(d1, e1, f1, T, singular1, B_val1, S1);
-        returnXValue = CalculateValues(S1, t, T, SW_x_case);
-
-        //// Solve z ////
-        // solve undetermined coefficients (double S2[6] = {a2, b2, c2, d2, e2, f2};)
-        solve(d2, e2, f2, T, singular2, B_val2, S2);
-        returnZValue = CalculateValues(S2, t, T, SW_z_case);
-
-        } else {
-        ////////////////// ReversePhase //////////////////
-        returnXValue = CalculateValues(S1, t, T, Reverse_x_case);
-        returnZValue = CalculateValues(S2, t, T, Reverse_z_case);
-        }
-        xVal = returnXValue;
-        zVal = returnZValue;
     }
 
     ////////////////////////////////////////////////////////
@@ -217,7 +163,7 @@ private:
 
     double CalculateKinematics(double xVal, double zVal, int cases)
     {
-        double returnDegree;
+        // double returnDegree;
         double len_hip = 250, len_knee = 250;
 
         zVal = -zVal;
@@ -229,29 +175,64 @@ private:
         // Calculate Hip Joint Value using Inverse Kinematics
         double hip_degree = atan2(zVal, xVal) - atan2(len_knee*sin(knee_degree), len_hip + len_knee*cos(knee_degree));
 
-        knee_degree = knee_degree - M_PI_2;
+        knee_degree -= M_PI_2;
 
         if (cases == 1)
-            returnDegree = hip_degree;
+            return hip_degree;
         else
-            returnDegree = knee_degree;
+            return knee_degree;
 
-        return returnDegree;
+        // return returnDegree;
     }
 
-    double PID(double kp, double kd, double degree, int cases)
+    void SplineTrajectory(double sim_time, double &xVal, double &zVal)
     {
-        double returnValue;
+        /////////////////// Initializing ////////////////////
 
-        double output_torque_5 = kp*(degree - joint5_pos) + kd*(0 - joint5_vel);
-        double output_torque_6 = kp*(degree - joint6_pos) + kd*(0 - joint6_vel);
+        double vel_of_body = 1600;
+        double T = 0.5;
+        double length_of_STanding_phase = vel_of_body * T /2;
+        double height = 1656/5;
 
-        if (cases == 1)
-            returnValue = output_torque_5;
-        else
-            returnValue = output_torque_6;
+        int SW_x_case = 2, Reverse_x_case = 3;
+        int SW_z_case = 5, Reverse_z_case = 6;
 
-        return returnValue;
+        // undetermined coefficients (a1*t^5 + b1*t^4 + c1*t^3 + d1*t^2 + e1*t + f1)
+        double d1 = 0, e1 = -(vel_of_body), f1 = -length_of_STanding_phase/2;
+        double singular1 = T/16;
+        double B_val1[3] = {-e1, -(f1 +e1*T/4), 0};
+        double S1[6];
+
+        // undetermined coefficients (a2*t^5 + b2*t^4 + c2*t^3 + d2*t^2 + e2*t + f2)
+        double d2 = 0, e2 = 0, f2 = -height;
+        double singular2 = T/4;
+        double B_val2[3] = {0, -(5*height/6 +f2), 0};
+        double S2[6];
+
+        /////////////////////// Calculate the return Value ////////////////////////
+        // Initialize the return value
+        double returnXValue, returnZValue;
+        double t = fmod(sim_time, T);
+
+        if (t <= T/2) {
+            returnXValue = CalculateXValues(length_of_STanding_phase, vel_of_body, t);
+            returnZValue = -height;
+
+        } else if (t <= T/4*3) {
+            // solve undetermined coefficients (double S1[6] = {a1, b1, c1, d1, e1, f1};)
+            solve(d1, e1, f1, T, singular1, B_val1, S1);
+            returnXValue = CalculateValues(S1, t, T, SW_x_case);
+
+            // solve undetermined coefficients (double S2[6] = {a2, b2, c2, d2, e2, f2};)
+            solve(d2, e2, f2, T, singular2, B_val2, S2);
+            returnZValue = CalculateValues(S2, t, T, SW_z_case);
+
+        } else {
+            returnXValue = CalculateValues(S1, t, T, Reverse_x_case);
+            returnZValue = CalculateValues(S2, t, T, Reverse_z_case);
+        }
+        xVal = returnXValue;
+        zVal = returnZValue;
     }
 
     void CalculateAndPublishTorque()
@@ -260,11 +241,11 @@ private:
         double xVal, zVal;
         SplineTrajectory(sim_time, xVal, zVal);
 
-        double hip_degree = CalculateKinematics(xVal, zVal, 1);
-        double knee_degree = CalculateKinematics(xVal, zVal, 2);
+        double RF_hip_degree = CalculateKinematics(xVal, zVal, 1);
+        double RF_knee_degree = CalculateKinematics(xVal, zVal, 2);
 
-        double RF_hip_output_torque = PID(kp[1], kd[1], hip_degree, 1);
-        double RF_knee_output_torque = PID(kp[2], kd[2], knee_degree, 2);
+        // double RF_hip_output_torque = PID(kp[1], kd[1], hip_degree, 1);
+        // double RF_knee_output_torque = PID(kp[2], kd[2], knee_degree, 2);
 
         std_msgs::msg::Float32MultiArray torque_msg;
         torque_msg.data.clear();
@@ -273,8 +254,8 @@ private:
         torque_msg.data.push_back(0);
         torque_msg.data.push_back(0);
         torque_msg.data.push_back(-ag[0]);
-        torque_msg.data.push_back(RF_hip_output_torque);
-        torque_msg.data.push_back(RF_knee_output_torque);
+        torque_msg.data.push_back(kp[1]*(RF_hip_degree - joint5_pos) + kd[1]*(0-joint5_vel));
+        torque_msg.data.push_back(kp[2]*(RF_knee_degree - joint6_pos) + kd[2]*(0-joint6_vel));
         torque_msg.data.push_back(ag[0]);
         torque_msg.data.push_back(0);
         torque_msg.data.push_back(0);
@@ -292,8 +273,8 @@ private:
         desiredpos_msg.data.push_back(0);
         desiredpos_msg.data.push_back(0);
         desiredpos_msg.data.push_back(ag[0]);
-        desiredpos_msg.data.push_back(hip_degree);
-        desiredpos_msg.data.push_back(knee_degree);
+        desiredpos_msg.data.push_back(RF_hip_degree);
+        desiredpos_msg.data.push_back(RF_knee_degree);
         desiredpos_msg.data.push_back(ag[0]);
         desiredpos_msg.data.push_back(0);
         desiredpos_msg.data.push_back(0);
