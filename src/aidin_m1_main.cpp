05afe49733d4f1fef5f792b3ad6e57ed1ee7e787
eunseopsong <lexondms1@g.skku.edu>
2024-07-22 15:54:19 +0900

    aidin_m1_main.cpp 최적화 by chat GPT; torque 계산 수정 대기

--- src/aidin_m1_main.cpp
+++ src/aidin_m1_main.cpp
@@ -4,112 +4,73 @@ using namespace std;
 using namespace Eigen;
 using namespace std::chrono_literals;
 
-class JointControl: public rclcpp::Node
+class JointControl : public rclcpp::Node
 {
 public:
     JointControl()
-    : Node("aidin_m1_control_node"), count_(0) // count_ == 0 (Initialize)
+        : Node("aidin_m1_control_node"), count_(0)
     {
-        // Subscribe
-        sub_bodypose = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/BodyPose_sim", 10,
-            [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
-                for (int i = 0; i < 6; ++i) {
-                    body_pose[i] = msg->data[i];
-                }
+        // Subscribe to topics
+        sub_bodypose = create_subscription<std_msgs::msg::Float32MultiArray>(
+            "/aidin_m1/BodyPose_sim", 10, [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+                copy(msg->data.begin(), msg->data.begin() + 6, body_pose.begin());
             });
-
-        sub_jointpos = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/JointPos_sim", 10,
-            [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
-                for (int i = 0; i < 12; ++i) {
-                    joint_pos[i] = msg->data[i];
-                }
+        sub_jointpos = create_subscription<std_msgs::msg::Float32MultiArray>(
+            "/aidin_m1/JointPos_sim", 10, [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+                copy(msg->data.begin(), msg->data.begin() + 12, joint_pos.begin());
             });
-
-        sub_jointvel = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/JointVel_sim", 10,
-            [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
-                for (int i = 0; i < 12; ++i) {
-                    joint_vel[i] = msg->data[i];
-                }
+        sub_jointvel = create_subscription<std_msgs::msg::Float32MultiArray>(
+            "/aidin_m1/JointVel_sim", 10, [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+                copy(msg->data.begin(), msg->data.begin() + 12, joint_vel.begin());
             });
-
-        sub_bodypos = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/BodyPos_sim", 10,
-            [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
-                for (int i = 0; i < 3; ++i) {
-                    body_pos[i] = msg->data[i];
-                }
+        sub_bodypos = create_subscription<std_msgs::msg::Float32MultiArray>(
+            "/aidin_m1/BodyPos_sim", 10, [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+                copy(msg->data.begin(), msg->data.begin() + 3, body_pos.begin());
             });
-
-        sub_bodyvel = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/BodyVel_sim", 10,
-            [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
-                for (int i = 0; i < 3; ++i) {
-                    body_vel[i] = msg->data[i];
-                }
+        sub_bodyvel = create_subscription<std_msgs::msg::Float32MultiArray>(
+            "/aidin_m1/BodyVel_sim", 10, [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+                copy(msg->data.begin(), msg->data.begin() + 3, body_vel.begin());
             });
-
-        sub_imu = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/IMU_sim", 10,
-            [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
-                for (int i = 0; i < 9; ++i) {
-                    imu[i] = msg->data[i];
-                }
+        sub_imu = create_subscription<std_msgs::msg::Float32MultiArray>(
+            "/aidin_m1/IMU_sim", 10, [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+                copy(msg->data.begin(), msg->data.begin() + 9, imu.begin());
             });
-
-        sub_contact = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/Contact_sim", 10,
-            [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
-                for (int i = 0; i < 4; ++i) {
-                    contact[i] = msg->data[i];
-                }
+        sub_contact = create_subscription<std_msgs::msg::Float32MultiArray>(
+            "/aidin_m1/Contact_sim", 10, [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+                copy(msg->data.begin(), msg->data.begin() + 4, contact.begin());
             });
-
-        sub_gains = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/Gains_sim", 10,
-            [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
-                for (int i = 0; i < 6; ++i) {
+        sub_gains = create_subscription<std_msgs::msg::Float32MultiArray>(
+            "/aidin_m1/Gains_sim", 10, [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+                for (int i = 0; i < 6; ++i)
+                {
                     if (i <= 2)
-                        Kp[i]   = msg->data[i];
+                        Kp[i] = msg->data[i];
                     else
-                        Kd[i-3] = msg->data[i];
+                        Kd[i - 3] = msg->data[i];
                 }
             });
-
-        sub_angles = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/Angles_sim", 10,
-            [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
-                for (int i = 0; i < 3; ++i) {
-                    angle[i] = msg->data[i];
-                }
+        sub_angles = create_subscription<std_msgs::msg::Float32MultiArray>(
+            "/aidin_m1/Angles_sim", 10, [this](const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+                copy(msg->data.begin(), msg->data.begin() + 3, angle.begin());
             });
 
         // Publish torque & target joint poses
-        pub_torque = this->create_publisher<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/Torque_sim", 10);
-
-        pub_targetpos = this->create_publisher<std_msgs::msg::Float32MultiArray>(
-            "/aidin_m1/TargetPos", 10);
+        pub_torque = this->create_publisher<std_msgs::msg::Float32MultiArray>("/aidin_m1/Torque_sim", 10);
+        pub_targetpos = this->create_publisher<std_msgs::msg::Float32MultiArray>("/aidin_m1/TargetPos", 10);
 
         // node Publish 실행 주기 설정 (1ms)
-        timer_ = this->create_wall_timer(
-            1ms, std::bind(&JointControl::CalculateAndPublishTorque, this));
+        timer_ = this->create_wall_timer(1ms, std::bind(&JointControl::CalculateAndPublishTorque, this));
     }
 
 private:
-    /////////////// timer_에 의해 호출되어 Publish를 실행하는 함수 ///////////////
-
     void CalculateAndPublishTorque()
     {
-        // Initialization
-        count_ = count_ + 0.001; // CalculateAndPublishTorque가 실행될 때마다 count_ = count + 1ms; -> count_ 는 실제 시뮬레이션 시간을 나타내는 변수가 됨
+        count_ += 0.001; // Increment simulation time by 1ms
 
-        double vel_of_body = 500;    // The target velocity of whole robot bodys
-        double T = 1.6;               // The period of the whole trajectory phase
+        double vel_of_body = 500; // Target velocity of the whole robot body
+        double T = 1.6;           // Period of the whole trajectory phase
         double t = fmod(count_, T);
-        double t_counter = fmod(count_ + 0.800 , T);
+        double t_counter = fmod(count_ + 0.800, T);
 
         // Calculate the coordinate using Trajectory Function
         double yVal = 95;
@@ -119,113 +80,101 @@ private:
         double xVal_counter, zVal_counter;
         SplineTrajectory(t_counter, T, vel_of_body, xVal_counter, zVal_counter);
 
-        // Calulate the target_pos using Inverse Kinematics
-        double target_pos[12];
-        double LF_target_pos[3];
-        double RF_target_pos[3];
-        double LB_target_pos[3];
-        double RB_target_pos[3];
+        // Calculate the target_pos using Inverse Kinematics
+        array<double, 3> LF_target_pos, RF_target_pos, LB_target_pos, RB_target_pos;
 
-        InverseKinematics3D(yVal, zVal, xVal, yVal, 250, 250, LF_target_pos);
-        InverseKinematics3D(yVal, zVal_counter, xVal_counter, yVal, 250, 250, RF_target_pos);
-        InverseKinematics3D(yVal, zVal_counter, xVal_counter, yVal, 250, 250, LB_target_pos);
-        InverseKinematics3D(yVal, zVal, xVal, yVal, 250, 250, RB_target_pos);
+        InverseKinematics3D(yVal, zVal, xVal, yVal, 250, 250, LF_target_pos.data());
+        InverseKinematics3D(yVal, zVal_counter, xVal_counter, yVal, 250, 250, RF_target_pos.data());
+        InverseKinematics3D(yVal, zVal_counter, xVal_counter, yVal, 250, 250, LB_target_pos.data());
+        InverseKinematics3D(yVal, zVal, xVal, yVal, 250, 250, RB_target_pos.data());
 
-        for (int i=1; i<6; i++)
-        {
-            if (i<3)
-                target_pos[i] = LF_target_pos[i];
-            else if (i<6)
-                target_pos[i] = RF_target_pos[i-3];
-            else if (i<9)
-                target_pos[i] = LB_target_pos[i-6];
-            else
-                target_pos[i] = RB_target_pos[i-9];
-        }
+        vector<double> target_pos;
+        target_pos.insert(target_pos.end(), LF_target_pos.begin(), LF_target_pos.end());
+        target_pos.insert(target_pos.end(), RF_target_pos.begin(), RF_target_pos.end());
+        target_pos.insert(target_pos.end(), LB_target_pos.begin(), LB_target_pos.end());
+        target_pos.insert(target_pos.end(), RB_target_pos.begin(), RB_target_pos.end());
 
         // Calculate the output_torque using PD or Feedforward control
-        double output_torque[12];
+        vector<double> output_torque(12);
 
-        for (int i=0; i<12; i++)
+        for (int i = 0; i < 12; ++i)
         {
-
             if (Kp[0] > 0 && Kp[1] == 0) // standing command
             {
-                if (i < 3)    // LF joint
-                {
-                    LF_target_pos[0] = 0;
-                    LF_target_pos[1] = M_PI_2/3;
-                    LF_target_pos[2] = 0;
-                    output_torque[i] = FeedforwardController(Kp[i], Kd[i], LF_target_pos, i, 0);
-                }
-                else if (i < 6) // RF joint
-                {
-                    RF_target_pos[0] = 0;
-                    RF_target_pos[1] = M_PI_2/3;
-                    RF_target_pos[2] = 0;
-                    output_torque[i] = FeedforwardController(Kp[i-3], Kd[i-3], RF_target_pos, i-3, 3);
-                }
-                else if (i < 9) // LB joint
-                {
-                    LB_target_pos[0] = 0;
-                    LB_target_pos[1] = M_PI_2/3;
-                    LB_target_pos[2] = 0;
-                    output_torque[i] = FeedforwardController(Kp[i-6], Kd[i-6], LB_target_pos, i-6, 6);
-                }
-                else
-                {
-                    RB_target_pos[0] = 0;
-                    RB_target_pos[1] = M_PI_2/3;
-                    RB_target_pos[2] = 0;
-                    output_torque[i] = FeedforwardController(Kp[i-9], Kd[i-9], RB_target_pos, i-9, 9);
-                }
+                output_torque[i] = calculate_torque_standing(i, LF_target_pos, RF_target_pos, LB_target_pos, RB_target_pos);
             }
             else if (Kp[0] > 0 && Kp[1] > 0) // running command
             {
-                if (i<3)      // LF joint
-                {
-                    // output_torque[i] = PDController(Kp[i],   Kd[i],   target_pos[i], joint_pos[i], joint_vel[i]);
-                    output_torque[i] = FeedforwardController(Kp[i], Kd[i], LF_target_pos, i, 0);
-                }
-                else if (i<6) // RF joint
-                {
-                    // output_torque[i] = PDController(Kp[i-3], Kd[i-3], target_pos[i], joint_pos[i], joint_vel[i]);
-                    output_torque[i] = FeedforwardController(Kp[i-3], Kd[i-3], RF_target_pos, i-3, 3);
-                }
-                else if (i<9) // LB joint
-                {
-                    output_torque[i] = FeedforwardController(Kp[i-6], Kd[i-6], LB_target_pos, i-6, 6);
-                }
-                else          // RB joint
-                {
-                    output_torque[i] = FeedforwardController(Kp[i-9], Kd[i-9], RB_target_pos, i-9, 9);
-                }
+                output_torque[i] = calculate_torque_running(i, LF_target_pos, RF_target_pos, LB_target_pos, RB_target_pos);
             }
             else
+            {
                 output_torque[i] = 0;
+            }
         }
 
-        /////////////// Publish Desired Pose ///////////////
+        // Publish Desired Pose
         std_msgs::msg::Float32MultiArray targetpos_msg;
-        targetpos_msg.data.clear();
-
-        for (int i=0; i < 12; i++){
-            targetpos_msg.data.push_back(target_pos[i]);
-        }
-
+        targetpos_msg.data.insert(targetpos_msg.data.end(), target_pos.begin(), target_pos.end());
         pub_targetpos->publish(targetpos_msg);
 
-        ////////////////// Publish Torque //////////////////
+        // Publish Torque
         std_msgs::msg::Float32MultiArray torque_msg;
-        torque_msg.data.clear();
-
-        for (int i=0; i < 12; i++){
-            torque_msg.data.push_back(output_torque[i]);
-            // torque_msg.data.push_back(0);
-        }
-        // torque_msg.data.push_back(90);
+        torque_msg.data.insert(torque_msg.data.end(), output_torque.begin(), output_torque.end());
         pub_torque->publish(torque_msg);
     }
+
+    double calculate_torque_standing(int i, array<double, 3> &LF_target_pos, array<double, 3> &RF_target_pos,
+                                     array<double, 3> &LB_target_pos, array<double, 3> &RB_target_pos)
+    {
+        double torque;
+        if (i < 3) // LF joint
+        {
+            LF_target_pos = {0, M_PI_2 / 3, 0};
+            torque = FeedforwardController(Kp[i], Kd[i], LF_target_pos.data(), i, 0);
+        }
+        else if (i < 6) // RF joint
+        {
+            RF_target_pos = {0, M_PI_2 / 3, 0};
+            torque = FeedforwardController(Kp[i - 3], Kd[i - 3], RF_target_pos.data(), i - 3, 3);
+        }
+        else if (i < 9) // LB joint
+        {
+            LB_target_pos = {0, M_PI_2 / 3, 0};
+            torque = FeedforwardController(Kp[i - 6], Kd[i - 6], LB_target_pos.data(), i - 6, 6);
+        }
+        else // RB joint
+        {
+            RB_target_pos = {0, M_PI_2 / 3, 0};
+            torque = FeedforwardController(Kp[i - 9], Kd[i - 9], RB_target_pos.data(), i - 9, 9);
+        }
+        return torque;
+    }
+
+    double calculate_torque_running(int i, array<double, 3> &LF_target_pos, array<double, 3> &RF_target_pos,
+                                    array<double, 3> &LB_target_pos, array<double, 3> &RB_target_pos)
+    {
+        double torque;
+        if (i < 3) // LF joint
+        {
+            torque = FeedforwardController(Kp[i], Kd[i], LF_target_pos.data(), i, 0);
+        }
+        else if (i < 6) // RF joint
+        {
+            torque = FeedforwardController(Kp[i - 3], Kd[i - 3], RF_target_pos.data(), i - 3, 3);
+        }
+        else if (i < 9) // LB joint
+        {
+            torque = FeedforwardController(Kp[i - 6], Kd[i - 6], LB_target_pos.data(), i - 6, 6);
+        }
+        else // RB joint
+        {
+            torque = FeedforwardController(Kp[i - 9], Kd[i - 9], RB_target_pos.data(), i - 9, 9);
+        }
+        return torque;
+    }
+
+    // Subscriber declarations
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_bodypose;
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_jointpos;
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_jointvel;
@@ -233,15 +182,28 @@ private:
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_bodyvel;
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_imu;
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_contact;
-
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_angles;
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_gains;
 
+    // Publisher declarations
     rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr pub_torque;
     rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr pub_targetpos;
 
+    // Timer
     rclcpp::TimerBase::SharedPtr timer_;
     double count_;
+
+    // Data storage
+    array<double, 6> body_pose{};
+    array<double, 12> joint_pos{};
+    array<double, 12> joint_vel{};
+    array<double, 3> body_pos{};
+    array<double, 3> body_vel{};
+    array<double, 9> imu{};
+    array<double, 4> contact{};
+    array<double, 3> angle{};
+    array<double, 3> Kp{};
+    array<double, 3> Kd{};
 };
 
 int main(int argc, char **argv)
