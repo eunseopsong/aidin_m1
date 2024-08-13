35b544bee58d1add26027d4ecce4ded100ff5bdb
eunseopsong <lexondms1@g.skku.edu>
2024-08-13 16:05:58 +0900

    Add build directory to .gitignore

--- build/src/AiDIN_Controller/CMakeFiles/aidin_ctrl.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for aidin_ctrl.
-# This may be replaced when dependencies are built.
--- build/src/gazebo_plugin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eunseop/dev_ws/src/aidin_m1/src/gazebo_plugin
-# Build directory: /home/eunseop/dev_ws/src/aidin_m1/build/src/gazebo_plugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
