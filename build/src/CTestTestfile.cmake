57db18c81a171904fff477df0822edb66f543769
eunseopsong <lexondms1@g.skku.edu>
2024-08-13 16:11:21 +0900

    remove build

--- build/src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eunseop/dev_ws/src/aidin_m1/src
-# Build directory: /home/eunseop/dev_ws/src/aidin_m1/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("AiDIN_Controller")
-subdirs("gazebo_plugin")
